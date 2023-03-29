5eb9375d3d74bd6778025012d4c3646b257d9312
Your Name <you@example.com>
2023-03-29 08:30:08 +0500

    C# Lessons

---
+++ 017_Битовые операции/BitwiseOperators/IntegerDisjunction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntegerDisjunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte port = 0b_0000_1011;   // 0B Hex = 11 Dec 
+            Console.WriteLine($"port = {port:X2} = {port}");
+
+            byte mask = 0b_0000_0100;   // 04 Hex = 4 Dec
+            Console.WriteLine($"mask = {mask:X2} = {mask}");
+
+            port = (byte)(port | mask); // 0F Hex = 15 Dec
+            Console.WriteLine($"port = {port:X2} = {port}");
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
